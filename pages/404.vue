13c8fb438060f4e060ceb9e0ecd6eef2540c78e4
jebediah47 <jebediah47@protonmail.com>
2022-08-08 11:43:10 +0300

    Added 404 handler again

---
+++ pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <Head>
+    <Title>404 Not found...</Title>
+  </Head>
+  <div class="hero min-h-screen bg-[url('/images/desert_van_unsplash.webp')]">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1
+          class="mb-5 text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-yellow-500"
+        >
+          404 Not found...
+        </h1>
+        <p class="mb-5 text-xl">
+          We apologize but... The page you have been looking for could not be
+          found.
+        </p>
+        <NuxtLink to="/" class="btn btn-outline btn-error"
+          >Go Back to Homepage</NuxtLink
+        >
+      </div>
+    </div>
+  </div>
+</template>
