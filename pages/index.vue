02322943b331af81abae3419602cd64b9b5642fb
jebediah47 <jebediah47@protonmail.com>
2022-08-09 18:49:31 +0300

    Added Buymeacoffee widget

--- pages/index.vue
+++ pages/index.vue
@@ -18,3 +18,32 @@
     <Projects />
   </div>
 </template>
+
+<!-- Buymeacoffee widget -->
+<script lang="ts">
+export default {
+  mounted() {
+    let buymeacoffee = document.createElement("script");
+    buymeacoffee.type = "text/javascript";
+    buymeacoffee.setAttribute("data-name", "BMC-Widget");
+    buymeacoffee.src =
+      "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js";
+    buymeacoffee.setAttribute("data-id", "jebediah47");
+    buymeacoffee.setAttribute(
+      "data-description",
+      "Support me on Buy me a coffee!"
+    );
+    buymeacoffee.setAttribute("data-message", "");
+    buymeacoffee.setAttribute("data-color", "#5F7FFF");
+    buymeacoffee.setAttribute("data-position", "Right");
+    buymeacoffee.setAttribute("data-x_margin", "18");
+    buymeacoffee.setAttribute("data-y_margin", "18");
+    buymeacoffee.async = true;
+    buymeacoffee.onload = function () {
+      let evt = new Event("DOMContentLoaded");
+      window.dispatchEvent(evt);
+    };
+    document.body.appendChild(buymeacoffee);
+  },
+};
+</script>
