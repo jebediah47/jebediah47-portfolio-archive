7822b8a22a4eed09113a089e6c916b2c5984279d
Christian Llupo <jebediah47@protonmail.com>
2022-09-18 13:39:42 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   telemetry: false,
-  ssr: false,
+  ssr: true,
   app: {
     head: {
       charset: "utf-8",
