176a25b74c1bb5263d1e61dfcbbda1718a40e0b0
Christian Llupo <jebediah47@protonmail.com>
2022-09-18 18:10:54 +0300

    hewo

--- error.vue
+++ error.vue
@@ -1,17 +1,10 @@
 <template>
   <div
-    class="hero min-h-screen bg-base-200 bg-[url('./public/images/desert_van_unsplash.webp')]"
+    class="hero min-h-screen bg-base-200 bg-[url('./assets/webp/desert_van_unsplash.webp')]"
   >
     <div class="hero-content text-center">
       <div class="max-w-md">
         <h1
-          v-if="error.statusCode === 404"
-          class="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
-        >
-          Page not found
-        </h1>
-        <h1
-          v-else
           class="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
         >
           An error occurred {{ error.statusCode }}
